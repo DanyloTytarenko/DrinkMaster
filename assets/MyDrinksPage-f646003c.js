9d6c70250e91f0fada46e01b3dde030efaa04d67
DanyloTytarenko <121917694+DanyloTytarenko@users.noreply.github.com>
2023-10-27 18:24:24 +0000

    Deploying to gh-pages from @ DanyloTytarenko/DrinkMaster@bbbebd5ed63861ba33c98ea87bccb57e860b4e38 🚀

---
+++ assets/MyDrinksPage-f646003c.js
@@ -0,0 +1,14 @@
+import{n as t,j as e}from"./index-82d7ab06.js";const f=t.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-top: 500px;
+`,n=t.h1`
+  color: #ffffff;
+  text-shadow:
+    0 0 5px #00baff,
+    0 0 10px #00baff,
+    0 0 20px #00baff,
+    0 0 40px #00baff,
+    0 0 80px #00baff;
+`,s=()=>e.jsx(f,{children:e.jsx(n,{children:"DrinksPage Page"})});export{s as default};
