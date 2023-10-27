83547937939f47c99ae52a5439b394b2def9dc06
CodeOlga <111949555+CodeOlga@users.noreply.github.com>
2023-10-27 18:48:59 +0300

    add base navigation

---
+++ src/pages/MyDrinksPage/MyDrinksPage.jsx
@@ -0,0 +1,11 @@
+import { Container, Title } from './MyDrinksPage.styled';
+
+const MyDrinksPage = () => {
+  return (
+    <Container>
+      <Title>DrinksPage Page</Title>
+    </Container>
+  );
+};
+
+export default MyDrinksPage;
