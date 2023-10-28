1d92f9ca63aca6f8da2af5d4983c8d2e0f51182c
Danylo Tytarenko <121917694+DanyloTytarenko@users.noreply.github.com>
2023-10-28 20:31:45 +0300

    add provider

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,16 @@ import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+
+import { persistor, store } from '../redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
-import { store } from '../redux/store.js';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
     <BrowserRouter basename="/DrinkMaster">
-      <App />
-      </BrowserRouter>
-    </Provider>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>,
 );
