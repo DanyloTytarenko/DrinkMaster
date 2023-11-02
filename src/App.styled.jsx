6ff50307c346187856b851b84569975849f2109a
CodeOlga <111949555+CodeOlga@users.noreply.github.com>
2023-11-02 11:36:38 +0200

    Update App.styled.jsx

--- src/App.styled.jsx
+++ src/App.styled.jsx
@@ -1,14 +1,17 @@
 import styled from '@emotion/styled';
-import { colors } from './colors';
-// import example from './assets/background.png';
+import example from './assets/background.png';
 
 export const AppWrapper = styled.div`
   width: 100%;
   height: 100vh;
-  background-color: ${colors.primaryDark};
+  background: linear-gradient(
+    180deg,
+    rgba(0, 0, 0, 0.6) 58.58%,
+    rgba(0, 0, 0, 0.345172) 78.98%,
+    rgba(0, 0, 0, 0) 100%
+  );
+
   background-repeat: no-repeat;
   background-position: left top;
   background-size: 100% 100%;
 `;
-
-// background-image: url(${ example });
