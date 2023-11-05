2efc39ca51d640233a071bc4bc7f8eae8b60893b
Rostykfps <rostikfps@gmail.com>
2023-11-05 18:14:35 +0200

    Changed import names

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,5 +1,5 @@
-import notFoundDeck from '../../assets/images/notFound/img-blue-iced-tea-desktop.png';
-import notFoundDeck2 from '../../assets/images/notFound/img-blue-iced-tea-desktop@2x.png';
+import notFoundDesk from '../../assets/images/notFound/img-blue-iced-tea-desktop.png';
+import notFoundDesk2 from '../../assets/images/notFound/img-blue-iced-tea-desktop@2x.png';
 import notFoundMobile from '../../assets/images/notFound/img-blue-iced-tea-mobile.png';
 import notFoundMobile2 from '../../assets/images/notFound/img-blue-iced-tea-mobile@2x.png';
 import { NotFoundWrapper, TextMessage } from './NotFound.styles';
@@ -10,13 +10,13 @@ const NotFound = ({ message }) => {
       <picture>
         <source
           media="(min-width: 768px)"
-          srcSet={`${notFoundDeck} 1x, ${notFoundDeck2} 2x`}
+          srcSet={`${notFoundDesk} 1x, ${notFoundDesk2} 2x`}
         />
         <source
           media="(min-width: 375px)"
           srcSet={`${notFoundMobile} 1x, ${notFoundMobile2} 2x`}
         />
-        <img src={notFoundDeck} alt="not found" />
+        <img src={notFoundDesk} alt="not found" />
       </picture>
       <TextMessage>{message}</TextMessage>
     </NotFoundWrapper>
