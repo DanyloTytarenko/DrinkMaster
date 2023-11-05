d6d59e8e749af6de6a3ce918bef4bf4d2d62d824
CodeOlga <111949555+CodeOlga@users.noreply.github.com>
2023-11-05 23:31:41 +0200

    bugfixed mainContainer

--- src/components/Footer/Footer.styled.jsx
+++ src/components/Footer/Footer.styled.jsx
@@ -11,28 +11,6 @@ export const FooterContainer = styled.footer`
   }
 `;
 
-export const FooterWrap = styled.div`
-  margin: 0 auto;
-  padding-right: 20px;
-  padding-left: 20px;
-
-  @media screen and (min-width: 375px) {
-    max-width: 375px;
-  }
-
-  @media screen and (min-width: 768px) {
-    max-width: 768px;
-    padding-right: 32px;
-    padding-left: 32px;
-  } 
-
-  @media screen and (min-width: 1440px) {
-    max-width: 1440px;
-    padding-right: 100px;
-    padding-left: 100px;
-  }
-`;
-
 export const MainWrap = styled.div`
   @media screen and (min-width: 768px) {
     display: flex;
