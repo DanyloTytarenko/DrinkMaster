4264c8ffe402f78b7ef84d9e4e7fcafe69eebecf
Rostykfps <rostikfps@gmail.com>
2023-11-05 21:05:58 +0200

    Add ErrorPage

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -5,26 +5,38 @@ import errorImgDesk from '../../assets/images/errorPage/img-blue-iced-tea-error-
 import errorImgDesk2 from '../../assets/images/errorPage/img-blue-iced-tea-error-desktop@2x.png';
 import errorImgMobile from '../../assets/images/errorPage/img-blue-iced-tea-error-mobile.png';
 import errorImgMobile2 from '../../assets/images/errorPage/img-blue-iced-tea-error-mobile@2x.png';
+import {
+  ErrorContainer,
+  ErrorWrapper,
+  StyledFirstSymbol,
+  StyledSecondSymbol,
+} from './ErrorPage.styled';
 
 const ErrorPage = () => {
   return (
     <>
       <Header />
-      <div>
-        <span>4</span>
-        <picture>
-          <source
-            media="(min-width: 768px)"
-            srcSet={`${errorImgDesk} 1x, ${errorImgDesk2} 2x`}
-          />
-          <source
-            media="(min-width: 375px)"
-            srcSet={`${errorImgMobile} 1x, ${errorImgMobile2} 2x`}
-          />
-          <img src={errorImgDesk} alt="error 404" />
-        </picture>
-        <span>4</span>
-      </div>
+      <ErrorContainer>
+        <ErrorWrapper>
+          <StyledFirstSymbol>4</StyledFirstSymbol>
+          <picture>
+            <source
+              media="(min-width: 768px)"
+              srcSet={`${errorImgDesk} 1x, ${errorImgDesk2} 2x`}
+              width="158"
+              height="201"
+            />
+            <source
+              media="(min-width: 375px)"
+              srcSet={`${errorImgMobile} 1x, ${errorImgMobile2} 2x`}
+              width="123"
+              height="156"
+            />
+            <img src={errorImgDesk} alt="error 404" width="123" height="156" />
+          </picture>
+          <StyledSecondSymbol>4</StyledSecondSymbol>
+        </ErrorWrapper>
+      </ErrorContainer>
       <Footer />
     </>
   );
