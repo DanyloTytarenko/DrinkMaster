0a16235e499f11fefe5c87d3a5d4b6797aed167f
OlexaGit <109541565+OlexaGit@users.noreply.github.com>
2023-11-06 05:30:28 +0200

    update button.jsx

--- src/components/DrinkDetails/Button/Button.jsx
+++ src/components/DrinkDetails/Button/Button.jsx
@@ -1,18 +1,22 @@
 import { useEffect, useState } from 'react';
 
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchFavoriteDrinks } from 'src/redux/drinks/operations';
+// import { fetchFavoriteDrinks } from 'src/redux/drinks/operations';
 import { StyledButton } from './Button.styled';
-import { selectFavoriteDrinks } from 'src/redux/drinks/selectors';
+// import { selectFavoriteDrinks } from 'src/redux/drinks/selectors';
 
 const Button = (drinkId) => {
   const [favorite, setfavorite] = useState(true);
+
   console.log(favorite);
+  // console.log(favoriteStatus);
+  console.log(drinkId);
+
   const handleAddToFavorite = () => setfavorite(true);
   const handleRemoveToFavorite = () => setfavorite(false);
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
   // const dataFavoriteDrinks = useSelector(selectFavoriteDrinks);
-  const { favoriteDrinks } = useSelector(selectFavoriteDrinks);
+  // const { favoriteDrinks } = useSelector(selectFavoriteDrinks);
   // const handleAddToFavorite = (id) => dispatch(AddFavorite(id));
   // const handleRemoveToFavorite = (id) => dispatch(RemoveFavorite(id));
 
@@ -22,27 +26,27 @@ const Button = (drinkId) => {
   //   });
   //   // setfavorite(false);
   // }
+  // console.log(favoriteDrinks);
+  // useEffect(() => {
+  //   dispatch(fetchFavoriteDrinks());
+  //   console.log(favoriteDrinks);
 
-  useEffect(() => {
-    dispatch(fetchFavoriteDrinks());
-    console.log(favoriteDrinks);
+  //   // if (favoriteDrinks.length == 0) {
+  //   //   return setfavorite(false);
+  //   // }
+  //   // else {
+  //   //   resulte = dataFavoriteDrinks.favoriteDrinks.find(
+  //   //     (option) => option.label === 'white',
+  //   //   ); // undefined
+  //   // }
 
-    if (favoriteDrinks.length == 0) {
-      return setfavorite(false);
-    }
-    // else {
-    //   resulte = dataFavoriteDrinks.favoriteDrinks.find(
-    //     (option) => option.label === 'white',
-    //   ); // undefined
-    // }
-
-    // setfavorite(false);
-  }, []);
+  //   // setfavorite(false);
+  // }, []);
 
   // function handleAddToFavorite() {
   //     setfavorite(false);
   //   }
-  console.log(drinkId);
+
   //   function handleRemoveToFavorite() {
   //     setfavorite(true);
   //   }
