35dbddb8d8ee131b4102afd60de3c4fd7b5888f7
Inna-Kulish <norelajest@gmail.com>
2023-11-07 19:41:09 +0200

    welcome page gradient

--- src/pages/SinginPage/SinginPage.jsx
+++ src/pages/SinginPage/SinginPage.jsx
@@ -1,12 +1,20 @@
-import { SinginForm } from "../../components/SinginForm/SinginForm"
-import { Title } from "../SingupPage/SingupPage.styled"
-import { WelcomeBox } from "../WelcomePage/WelcomePage.styled"
+import { SinginForm } from '../../components/SinginForm/SinginForm';
+import { Title } from '../SingupPage/SingupPage.styled';
+import {
+  WelcomeBox,
+  StyledBGElement6,
+  StyledBGElement7,
+  StyledBGElement8,
+} from '../WelcomePage/WelcomePage.styled';
 
 export default function SinginPage() {
-    return (
-        <WelcomeBox>
-            <Title>Sign In</Title>
-            <SinginForm/>
-        </WelcomeBox>
-    )
+  return (
+    <WelcomeBox>
+      <Title>Sign In</Title>
+      <SinginForm />
+      <StyledBGElement6 />
+      <StyledBGElement7 />
+      <StyledBGElement8 />
+    </WelcomeBox>
+  );
 }
--- src/pages/WelcomePage/WelcomePage.styled.jsx
+++ src/pages/WelcomePage/WelcomePage.styled.jsx
@@ -5,6 +5,7 @@ import welcomeCocktailTab from '../../assets/images/welcomePage/cocktail-tab@1x.
 import welcomeCocktailTab2 from '../../assets/images/welcomePage/cocktail-tab@2x.jpg';
 import welcomeCocktailMob from '../../assets/images/welcomePage/cocktail-mob@1x.jpg';
 import welcomeCocktailMob2 from '../../assets/images/welcomePage/cocktail-mob@2x.jpg';
+import desk from '../../assets/images/welcomePage/desk-2x.png'
 import { colors } from 'src/colors';
 
 export const WelcomeBox = styled.div`
@@ -19,7 +20,6 @@ export const WelcomeBox = styled.div`
   padding-right: 20px;
   padding-left: 20px;
 
-  /* background-color: black; */
   background-repeat: no-repeat;
   background-position: right top;
 
@@ -29,8 +29,8 @@ export const WelcomeBox = styled.div`
       rgba(10, 10, 17, 0.2) 0%,
       rgba(10, 10, 17, 0.2) 100%
     ),
-    linear-gradient(1deg, #0a0a11 -0.56%, rgba(10, 10, 17, 0) 21.93%),
-    linear-gradient(85deg, #060609 5.92%, rgba(10, 10, 17, 0) 51.07%),
+    linear-gradient(91deg, #0a0a11 -0.56%, rgba(10, 10, 17, 0) 51.93%),
+        linear-gradient(0deg, #0a0a11 11.92%, rgba(10, 10, 17, 0) 91.07%),
     url(${welcomeCocktailMob});
 
   @media (min-device-pixel-ratio: 2),
@@ -41,8 +41,8 @@ export const WelcomeBox = styled.div`
         rgba(10, 10, 17, 0.2) 0%,
         rgba(10, 10, 17, 0.2) 100%
       ),
-      linear-gradient(1deg, #0a0a11 -0.56%, rgba(10, 10, 17, 0) 21.93%),
-      linear-gradient(85deg, #060609 5.92%, rgba(10, 10, 17, 0) 51.07%),
+      linear-gradient(91deg, #0a0a11 -0.56%, rgba(10, 10, 17, 0) 51.93%),
+        linear-gradient(0deg, #060609 5.92%, rgba(10, 10, 17, 0) 91.07%),
       url(${welcomeCocktailMob2});
   }
 
@@ -60,8 +60,8 @@ export const WelcomeBox = styled.div`
         rgba(10, 10, 17, 0.2) 0%,
         rgba(10, 10, 17, 0.2) 100%
       ),
-      linear-gradient(1deg, #0a0a11 -0.56%, rgba(10, 10, 17, 0) 21.93%),
-      linear-gradient(85deg, #060609 5.92%, rgba(10, 10, 17, 0) 51.07%),
+      linear-gradient(91deg, #0a0a11 -0.56%, rgba(10, 10, 17, 0) 51.93%),
+        linear-gradient(85deg, #060609 5.92%, rgba(10, 10, 17, 0) 91.07%),
       url(${welcomeCocktailTab});
 
     @media (min-device-pixel-ratio: 2),
@@ -72,10 +72,11 @@ export const WelcomeBox = styled.div`
           rgba(10, 10, 17, 0.2) 0%,
           rgba(10, 10, 17, 0.2) 100%
         ),
-        linear-gradient(1deg, #0a0a11 -0.56%, rgba(10, 10, 17, 0) 21.93%),
-        linear-gradient(85deg, #060609 5.92%, rgba(10, 10, 17, 0) 51.07%),
+        linear-gradient(91deg, #0a0a11 -0.56%, rgba(10, 10, 17, 0) 51.93%),
+        linear-gradient(85deg, #060609 5.92%, rgba(10, 10, 17, 0) 91.07%),
         url(${welcomeCocktailTab2});
     }
+
   }
 
   @media screen and (min-width: 1440px) {
@@ -90,26 +91,27 @@ export const WelcomeBox = styled.div`
         rgba(10, 10, 17, 0.2) 0%,
         rgba(10, 10, 17, 0.2) 100%
       ),
-      linear-gradient(1deg, #0a0a11 -0.56%, rgba(10, 10, 17, 0) 21.93%),
-      linear-gradient(85deg, #060609 5.92%, rgba(10, 10, 17, 0) 51.07%),
+      linear-gradient(91deg, #0a0a11 -0.56%, rgba(10, 10, 17, 0) 51.93%),
+        linear-gradient(85deg, #060609 5.92%, rgba(10, 10, 17, 0) 91.07%),
       url(${welcomeCocktailDesk});
 
     @media (min-device-pixel-ratio: 2),
       (min-resolution: 192dpi),
       (min-resolution: 2dppx) {
-      background-image: 
-      /* linear-gradient(
+      background-image: linear-gradient(
           0deg,
           rgba(10, 10, 17, 0.2) 0%,
           rgba(10, 10, 17, 0.2) 100%
         ),
-        linear-gradient(1deg, #0a0a11 -0.56%, rgba(10, 10, 17, 0) 21.93%),
-        linear-gradient(85deg, #060609 5.92%, rgba(10, 10, 17, 0) 51.07%), */ url(${welcomeCocktailDesk2});
+        linear-gradient(91deg, #0a0a11 -0.56%, rgba(10, 10, 17, 0) 51.93%),
+        linear-gradient(85deg, #060609 5.92%, rgba(10, 10, 17, 0) 91.07%),
+        url(${welcomeCocktailDesk2});
     }
   }
 `;
 
 export const Title = styled.h1`
+  z-index: 5;
   margin-bottom: 14px;
   color: ${colors.light};
   font-family: Manrope;
@@ -132,7 +134,7 @@ export const WelcomeText = styled.p`
   line-height: calc(18 / 14);
 
   color: ${colors.light};
-
+  z-index: 5;
   @media screen and (max-width: 767px) {
     width: 319px;
     text-align: center;
@@ -147,49 +149,80 @@ export const WelcomeText = styled.p`
 
 export const StyledBGElement6 = styled.div`
   position: absolute;
+  border-radius: 387px;
   background-color: rgba(188, 230, 210, 0.4);
   filter: blur(105px);
-  z-index: 1;
+  z-index: 2;
   width: 257px;
   height: 247px;
-  top: -52px;
-  right: 254px;
+  top: 52px;
+  left: 54px;
 
   @media screen and (min-width: 768px) {
     width: 387px;
     height: 373px;
     top: 46px;
     left: 0px;
+
   }
 
   @media screen and (min-width: 1440px) {
     width: 387px;
-    height: 381px;
-    top: -132px;
-    left: 171px;
+    height: 251px;
+    top: 0;
+    left: 101px;
   }
 `;
 export const StyledBGElement7 = styled.div`
   position: absolute;
+  border-radius: 550px;
   background-color: rgba(64, 112, 205, 0.5);
   filter: blur(105px);
   z-index: 1;
-  width: 520px;
-  height: 550px;
+  width: 120px;
+  height: 526px;
   top: 20px;
-  left: -399px;
+  left: -50px;
 
   @media screen and (min-width: 768px) {
-    width: 784px;
-    height: 830px;
-    bottom: 245px;
+    width: 150px;
+    height: 650px;
+    top: 0;
     right: 649px;
+
+    border-radius: 830px;
   }
 
   @media screen and (min-width: 1440px) {
-    width: 784px;
-    height: 849px;
-    bottom: 152px;
-    right: 1150px;
+    width: 304px;
+    height: 600px;
+    top: 0;
+    right: 650px;
+  }
+`;
+
+export const StyledBGElement8 = styled.div`
+  position: absolute;
+  border-radius: 549px;
+  background-color: rgba(64, 112, 205, 0.5);
+  filter: blur(105px);
+  z-index: 1;
+  width: 150px;
+  height: 250px;
+  bottom: 0;
+  right: 0;
+
+  @media screen and (min-width: 768px) {
+    width: 620px;
+  height: 350px;
+  bottom: 20px;
+  right: -399px;
+  }
+
+  @media screen and (min-width: 1440px) {
+    width: 520px;
+  height: 550px;
+  bottom: 20px;
+  right: -399px;
   }
 `;
