93ba4d92e36f66bb701250d149bae784b0f4cee0
CodeOlga <111949555+CodeOlga@users.noreply.github.com>
2023-11-07 22:29:29 +0200

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ import ThemeToggler from '../ThemeToggler/ThemeToggler';
 import UserLogo from '../UserLogo/UserLogo';
 import { BurgerMenu } from './BurgerMenu/BurgerMenu';
 import {
-  HeaderContainer, HeaderWrap
+  HeaderContainer
 } from './Header.styled';
 import { DropDown } from '../Modal/DropDown';
 const Header = () => {
@@ -31,22 +31,72 @@ const Header = () => {
 
   return (
     <HeaderContainer>
-      <HeaderWrap>
-        <Logo />
-        {windowWidth >= 1440 && <>
-          <Navigation />
-          <ThemeToggler />
-          <DropDown />
-        </>
-        }
-        {windowWidth < 1440 && <>
-          {isMenuOpen ? <ThemeToggler /> : <DropDown />}
-          <BurgerMenu isOpen={isMenuOpen} toggleMenu={toggleMenu} />
-        </>}
-      </HeaderWrap>
+      <Logo />
+      {windowWidth >= 1440 && <>
+        <Navigation />
+        <ThemeToggler />
+        <DropDown />
+      </>
+      }
+      {windowWidth < 1440 && <>
+        {isMenuOpen ? <ThemeToggler /> : <DropDown />}
+        <BurgerMenu isOpen={isMenuOpen} toggleMenu={toggleMenu} />
+      </>}
     </HeaderContainer>
   );
 };
 
 export default Header;
+
+// import { useState, useEffect } from 'react';
+// import Logo from '../Logo/Logo';
+// import Navigation from '../Navigation/Navigation';
+// import ThemeToggler from '../ThemeToggler/ThemeToggler';
+// import UserLogo from '../UserLogo/UserLogo';
+// import { BurgerMenu } from './BurgerMenu/BurgerMenu';
+// import {
+//   HeaderContainer, HeaderWrap
+// } from './Header.styled';
+// import { DropDown } from '../Modal/DropDown';
+// const Header = () => {
+//   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+//   const handleWindowResize = () => {
+//     setWindowWidth(window.innerWidth);
+//   };
+
+//   useEffect(() => {
+//     window.addEventListener('resize', handleWindowResize);
+
+//     return () => {
+//       window.removeEventListener('resize', handleWindowResize);
+//     };
+//   }, []);
+
+//   const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+//   const toggleMenu = () => {
+//     setIsMenuOpen(!isMenuOpen);
+//   };
+
+//   return (
+//     <HeaderContainer>
+//       <HeaderWrap>
+//         <Logo />
+//         {windowWidth >= 1440 && <>
+//           <Navigation />
+//           <ThemeToggler />
+//           <DropDown />
+//         </>
+//         }
+//         {windowWidth < 1440 && <>
+//           {isMenuOpen ? <ThemeToggler /> : <DropDown />}
+//           <BurgerMenu isOpen={isMenuOpen} toggleMenu={toggleMenu} />
+//         </>}
+//       </HeaderWrap>
+//     </HeaderContainer>
+//   );
+// };
+
+// export default Header;
 
