3aaa589b615a9d3f4aa126435841fa7ddf5d7343
Rostykfps <rostikfps@gmail.com>
2023-11-07 09:20:26 +0200

    Add backgraund styles to HomePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,6 +1,6 @@
 import Footer from 'src/components/Footer/Footer';
 import Header from 'src/components/Header/Header';
-import { Container } from './HomePage.styled';
+import { HomeContainer } from './HomePage.styled';
 import { AddDrink } from '../../components/HomePageComponents/AddDrink';
 import { PreviewDrinks } from '../../components/HomePageComponents/PreviewDrinks';
 
@@ -12,6 +12,7 @@ import {
 } from '../../redux/homepage/selectors.js';
 
 import { fetchNewDrinks } from '../../redux/homepage/operations.js';
+import MainAppContainer from 'src/components/MainAppContainer/MainAppContainer';
 
 const HomePage = () => {
   const dispatch = useDispatch();
@@ -31,12 +32,14 @@ const HomePage = () => {
   }
   return (
     <>
-      <Header></Header>
-      <Container>
-        <AddDrink />
-        <PreviewDrinks />
-      </Container>
-      <Footer></Footer>
+      <Header />
+      <MainAppContainer>
+        <HomeContainer>
+          <AddDrink />
+          <PreviewDrinks />
+        </HomeContainer>
+      </MainAppContainer>
+      <Footer />
     </>
   );
 };
--- src/pages/HomePage/HomePage.styled.jsx
+++ src/pages/HomePage/HomePage.styled.jsx
@@ -1,17 +1,21 @@
 import styled from '@emotion/styled';
 import { colors } from '../../colors';
 
-export const Container = styled.div`
+export const HomeContainer = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  padding: 80px 20px;
-  background-color: ${colors.primaryDark} @media only screen and
-    (min-width: 768px) {
-    padding: 120px 32px;
+  /* padding: 80px 20px; */
+  padding-top: 80px;
+  padding-bottom: 80px;
+  /* background-color: ${colors.primaryDark}; */
+  @media only screen and (min-width: 768px) {
+    /* padding: 120px 32px; */
+    padding-top: 120px;
+    padding-bottom: 120px;
   }
-  @media only screen and (min-width: 1440px) {
+  /* @media only screen and (min-width: 1440px) {
     padding: 0px 100px;
-  }
+  } */
 `;
