304e1136bf02ee3c3cf7e62982b40532ec3aef24
Danylo Tytarenko <121917694+DanyloTytarenko@users.noreply.github.com>
2023-11-07 22:43:14 +0200

    Update UserLogo.jsx

--- src/components/UserLogo/UserLogo.jsx
+++ src/components/UserLogo/UserLogo.jsx
@@ -1,19 +1,29 @@
-import { UserInfoWrap, UserLogoWrap, UserSvg, UserName } from './UserLogo.styled';
+import {
+  UserInfoWrap,
+  UserLogoWrap,
+  UserSvg,
+  UserName,
+} from './UserLogo.styled';
 import { useSelector } from 'react-redux';
-import { selectUser } from '../../redux/auth/selectors'
+import { selectUser } from '../../redux/auth/selectors';
 const UserLogo = () => {
   const user = useSelector(selectUser);
   return (
     <UserInfoWrap>
       <UserLogoWrap>
-        <img width="44" height="44" src={user.avatar || '/src/images/user.jpg'} alt="User avatar" onError={(event) => {
-                      event.currentTarget.src = '/src/images/user.jpg';
-                    }}/>
+        <img
+          width="44"
+          height="44"
+          src={user.avatar || '../../images/user.jpg'}
+          alt="User avatar"
+          onError={(event) => {
+            event.currentTarget.src = '/src/images/user.jpg';
+          }}
+        />
       </UserLogoWrap>
       <UserName>{user.name}</UserName>
     </UserInfoWrap>
-  )
-}
+  );
+};
 
 export default UserLogo;
-
